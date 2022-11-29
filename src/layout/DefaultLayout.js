7bd64c635d14935bc9d08d08c4595d1ab76db8ee
LDAP\dtdung1 <trungdunghbvn@gmail.com>
2022-11-29 17:06:56 +0700

    fix mobile

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useLayoutEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { AppContent, AppFooter, AppHeader, AppSidebar } from './component';
-import {classNames} from '../utilities/css';
+import { toggleSidebarAction } from '../reducers/appReducer';
+
+import { classNames } from '../utilities/css';
 import './Layout.scss';
 
 export default function DefaultLayout() {
+  const dispatch = useDispatch();
+
+  const [isMobile, setIsMobile] = useState(false);
+
   const isSidebar = useSelector(state => state.appReducer.isSidebar);
 
-  const siderbarClass = classNames(
-    isSidebar && 'Sidebar-Active'
-  );
+  const siderbarClass = classNames(isSidebar && 'Sidebar-Active');
+
+  const toggleSiderbar = () => {
+    dispatch(toggleSidebarAction());
+  };
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      const width = window.innerWidth;
+      if (!isMobile && width < 768) {
+        setIsMobile(true);
+      }
+      if (width >= 768 && isMobile) {
+        setIsMobile(false);
+      }
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, [isMobile, isSidebar]);
 
   return (
     <div className={siderbarClass}>
@@ -22,6 +47,9 @@ export default function DefaultLayout() {
         </div>
         <AppFooter />
       </div>
+      {isMobile && isSidebar && (
+        <div className="Sidebar-Backdrop" onClick={() => toggleSiderbar()} />
+      )}
     </div>
   );
 }
